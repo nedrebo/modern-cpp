5ebc978ada1654a51b80dd8b1ca6717769563ea3
Mathias Nedrebø <mathias.nedrebo@zividlabs.com>
2017-06-04 15:36:40 +0200

    Added ci build script for Windows

---
+++ ci/windows/build.cmd
@@ -0,0 +1,19 @@
+echo off
+
+set ORIGINAL_DIR=%CD%
+cd %~dp0
+
+mkdir build
+cd build && ^
+cmake ..\.. && ^
+msbuild ModernCpp.sln
+
+set BUILD_ERROR_CODE=%errorlevel%
+
+cd %ORIGINAL_DIR%
+
+if %BUILD_ERROR_CODE% neq 0 (
+    exit /b %BUILD_ERROR_CODE%
+)
+
+echo "Success!"
